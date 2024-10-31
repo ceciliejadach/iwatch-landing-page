c3d2dde619523fa0e45c13e66b4bf1c80c146661
Cecilie <cepe0001@stud.kea.dk>
2024-10-31 08:46:55 +0100

    stylet hover på knap

--- src/components/BuyNowButton.jsx
+++ src/components/BuyNowButton.jsx
@@ -1,5 +1,5 @@
 const BuyNowButton = () => {
-  return <button className="rounded-3xl text-xl border-solid border-white border-2 px-12 py-2 text-white w-fit h-fit">Buy Now</button>;
+  return <button className="rounded-3xl text-xl border-solid border-white border-2 px-12 py-2 text-white w-fit h-fit hover:bg-white hover:text-[#b6ccda]">Buy Now</button>;
 };
 
 export default BuyNowButton;
